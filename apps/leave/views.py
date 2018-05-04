6df4aa286ed86cfa73f86cc6c4a269c4c4842f26
yuchenfei <954833373@qq.com>
2018-05-04 17:52:04 +0800

    教师端请假管理分页显示，并微调了样式

--- apps/leave/views.py
+++ apps/leave/views.py
@@ -50,22 +50,36 @@ def list_list_p(request):
 
 
 def leave_list_t(request):
-    response_data = dict()
-    response_data['teacher'] = teacher = get_login_user(request)
-    if teacher.is_leader:
-        group = Group.objects.get(leader=teacher)
-        postgraduates = Postgraduate.objects.filter(Q(teacher=teacher) | Q(teacher__group=group)).all()
-    else:
-        postgraduates = Postgraduate.objects.filter(teacher=teacher).all()
-    if request.GET.get('show_all') == 'true':
+    if request.method == 'GET':
+        page = request.GET.get('page', '1')
+        teacher = get_login_user(request)
+        response = dict(teacher=teacher)
+        # 组长获取课题组所有研究生，普通教师获取自己的研究生
+        if teacher.is_leader:
+            group = Group.objects.get(leader=teacher)
+            postgraduates = Postgraduate.objects.filter(Q(teacher=teacher) | Q(teacher__group=group))
+        else:
+            postgraduates = Postgraduate.objects.filter(teacher=teacher)
+        # 是否显示已处理的记录
         leave_query = Leave.objects.filter(postgraduate__in=postgraduates)
-    else:
-        leave_query = Leave.objects.filter(state=None, postgraduate__in=postgraduates)
-    response_data['leave_list'] = leave_query.all()
-    return render(request, 'leave/leave_list_t.html', response_data)
+        leave_query = leave_query.order_by('-time_of_submission')
+        paginator = Paginator(leave_query, 5)
+        try:
+            leave_list = paginator.page(int(page))
+        except PageNotAnInteger:
+            leave_list = paginator.page(1)
+            page = 1
+        except EmptyPage:
+            leave_list = paginator.page(paginator.num_pages)
+            page = paginator.num_pages
+        response['leave_list'] = leave_list
+        response['range'] = paginator.page_range
+        response['page'] = int(page)
+        return render(request, 'leave/leave_list_t.html', response)
 
 
 def processes_leave(request, leave_id):
+    print(request.get_raw_uri())
     teacher = get_login_user(request)
     if request.method == 'POST':
         leave = Leave.objects.get(id=leave_id)
